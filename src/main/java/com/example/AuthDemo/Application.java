4e0e31ecb26a479a025744e33c14b138677ab847
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-07-27 12:05:15 -0700

    started work. password is not working

--- src/main/java/com/example/AuthDemo/Application.java
+++ src/main/java/com/example/AuthDemo/Application.java
@@ -4,16 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @GetMapping
